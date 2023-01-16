9daf1ca84544d5bdb99c3fe99586ec81e9e8c083
Dago <dagobert.harinck@student.howest.be>
2023-01-16 11:05:17 +0100

    get en post toegevoegd

--- app.js
+++ app.js
@@ -1,104 +1,102 @@
 import express from "express";
-import {createServer} from "http";
-import {normalizePort, onError, onListening} from './bin/serverconfig.js';
-import logger from 'morgan'
-import createError from 'http-errors';
-import {SerialSocket} from './bin/ESP32.js';
-import dotenv from 'dotenv';
-import {getKinectConnection} from "./bin/kinect.js";
-import fs from 'fs';
-import {WebSocketServer } from 'ws'
+import { createServer } from "http";
+import { normalizePort, onError, onListening } from "./bin/serverconfig.js";
+import logger from "morgan";
+import createError from "http-errors";
+import { SerialSocket } from "./bin/ESP32.js";
+import dotenv from "dotenv";
+import { getKinectConnection } from "./bin/kinect.js";
+import fs from "fs";
+import { WebSocketServer } from "ws";
+import { getResults, postResults } from "./bin/db.js";
 
 dotenv.config();
 
 export const app = express();
-export const port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+export const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
 export const httpServer = createServer(app);
 // add normal websocket support
-export const wss = new WebSocketServer({server: httpServer});
+export const wss = new WebSocketServer({ server: httpServer });
 
 let connectionCount = 0;
 
 wss.on("connection", (socket) => {
-    console.log("Connection established");
-    connectionCount += 1;
-    if(connectionCount > 1){
+  console.log("Connection established");
+  connectionCount += 1;
+  if (connectionCount > 1) {
+  }
+  const parser = SerialSocket(socket, process.env.ESP1_PORT, process.env.ESP1_BAUD);
+  parser.on("data", function (data) {
+    console.log("Data:", data);
+    socket.send(JSON.stringify({ data: data }));
+  });
+  const parser2 = SerialSocket(socket, process.env.ESP2_PORT, process.env.ESP2_BAUD);
+  parser2.on("data", function (data) {
+    console.log("Data2:", data);
+    socket.send(JSON.stringify({ data2: data }));
+  });
 
-    }
-    const parser = SerialSocket(socket, process.env.ESP1_PORT, process.env.ESP1_BAUD);
-    parser.on('data', function (data) {
-        console.log('Data:', data);
-        socket.send(JSON.stringify({data: data}));
-    });
-    const parser2 = SerialSocket(socket, process.env.ESP2_PORT, process.env.ESP2_BAUD);
-    parser2.on('data', function (data) {
-        console.log('Data2:', data);
-        socket.send(JSON.stringify({data2: data}));
-    });
-
-    const kinect = getKinectConnection();
-    socket.on('close', async () => {
-        await kinect.close();
-        connectionCount -=1;
-
-    })
-    kinect.on('bodyFrame', function (bodyFrame) {
-        for (let i = 0; i < bodyFrame.bodies.length; i++) {
-            if (bodyFrame.bodies[i].tracked) {
-                if (this.colorYmin === undefined) {
-                    this.colorYmin = bodyFrame.bodies[i].joints[0].colorY;
-                    this.colorYmax = bodyFrame.bodies[i].joints[0].colorY;
-                }
-                const spineShoulder = bodyFrame.bodies[i].joints[0];
-                // console.log(spineShoulder);
-                if (spineShoulder.colorY < this.colorYmin) {
-                    this.colorYmin = spineShoulder.colorY
-                }
-                if (spineShoulder.colorY > this.colorYmax) {
-                    this.colorYmax = spineShoulder.colorY
-                }
-                const diff = this.prevColorY - spineShoulder.colorY;
-                this.prevColorY = spineShoulder.colorY;
-                let msg = {min: this.colorYmin, max: this.colorYmax, diff: diff, shoulder: spineShoulder,};
-                console.log(this)
-                console.log(msg);
-                socket.send(JSON.stringify({kinect: msg}));
-                fs.appendFile('kinect.txt', `${spineShoulder.colorX}, ${spineShoulder.colorY}\n`, err => {
-                    console.log(err);
-                });
-            }
+  const kinect = getKinectConnection();
+  socket.on("close", async () => {
+    await kinect.close();
+    connectionCount -= 1;
+  });
+  kinect.on("bodyFrame", function (bodyFrame) {
+    for (let i = 0; i < bodyFrame.bodies.length; i++) {
+      if (bodyFrame.bodies[i].tracked) {
+        if (this.colorYmin === undefined) {
+          this.colorYmin = bodyFrame.bodies[i].joints[0].colorY;
+          this.colorYmax = bodyFrame.bodies[i].joints[0].colorY;
         }
-    });
+        const spineShoulder = bodyFrame.bodies[i].joints[0];
+        // console.log(spineShoulder);
+        if (spineShoulder.colorY < this.colorYmin) {
+          this.colorYmin = spineShoulder.colorY;
+        }
+        if (spineShoulder.colorY > this.colorYmax) {
+          this.colorYmax = spineShoulder.colorY;
+        }
+        const diff = this.prevColorY - spineShoulder.colorY;
+        this.prevColorY = spineShoulder.colorY;
+        let msg = { min: this.colorYmin, max: this.colorYmax, diff: diff, shoulder: spineShoulder };
+        console.log(this);
+        console.log(msg);
+        socket.send(JSON.stringify({ kinect: msg }));
+        fs.appendFile("kinect.txt", `${spineShoulder.colorX}, ${spineShoulder.colorY}\n`, (err) => {
+          console.log(err);
+        });
+      }
+    }
+  });
 });
 
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
-
-app.get('/', (req, res) => {
-    res.json('Hello World!');
+app.get("/", (req, res) => {
+  res.json("Hello World!");
 });
-
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    next(createError(404));
+  next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
 
+await postResults("test3", 100);
+
 httpServer.listen(3000);
-httpServer.on('error', onError);
-httpServer.on('listening', onListening);
+httpServer.on("error", onError);
+httpServer.on("listening", onListening);
--- bin/db.js
+++ bin/db.js
@@ -1,19 +1,24 @@
-import * as mysql from 'mysql2'
-import dotenv from 'dotenv'
-dotenv.config()
-const password = process.env.SQL_PASSWORD
+import * as mysql from "mysql2";
+import dotenv from "dotenv";
+dotenv.config();
+const password = process.env.SQL_PASSWORD;
 const config = {
-    host: "localhost",
-    user: "root",
-    password: password,
-    database: 'database'
+  host: "localhost",
+  user: "root",
+  password: password,
+  database: "groepprojdb",
 };
 
-export async function example1 () {
-    const conn = mysql.createPool(config);
-    const pool = conn.promise();
-    let [rows, fields] = await pool.query('SELECT * FROM statistics');
-    console.log(rows)
-    return rows
+export async function getResults() {
+  const conn = mysql.createPool(config);
+  const pool = conn.promise();
+  let [rows, fields] = await pool.query("SELECT * FROM statistics");
+  return rows;
 }
-example1()
+
+export async function postResults(name, score) {
+  const conn = mysql.createPool(config);
+  const pool = conn.promise();
+  let [rows, fields] = await pool.query("INSERT INTO statistics (username, score) VALUES (?, ?);", [name, score]);
+  return rows.affectedRows;
+}
