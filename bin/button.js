83fd413d4bfcab4a89ecad14502b395f1a7be171
jarneDel <jarne.delarue@student.howest.be>
2023-01-20 11:04:44 +0100

    fix button

--- bin/button.js
+++ bin/button.js
@@ -3,14 +3,12 @@ let btnState = {
     1: false,
 }
 
+let btn = [false, false, false]
+
+
 export function updateBtn(btnNumber, value) {
     btnState[btnNumber] = value
-    if (btnState[0] && btnState[1]){
-        return {btn: 'both'}
-    }
-    else if (btnState[0]){
-        return {btn: "left"}
-    }else if (btnState[1]){
-        return {btn: "right"}
-    }
+    btn[btnNumber] = value
+    btn[2] = btn[0] && btn[1];
+    return {btn: btn}
 }
