4828b9fa80d3f2162feab32409f2ed235dcf0fc9
jarneDel <jarne.delarue@student.howest.be>
2023-01-26 15:03:41 +0100

    fix: button on when socket connection starts

--- Arduino/YellowButton/YellowButton.ino
+++ Arduino/YellowButton/YellowButton.ino
@@ -8,13 +8,17 @@ String type = "right";
 bool btnUpdated = false;
 bool btnState = false;
 
+bool ledOn;
+unsigned long millisOld = 0;
+bool ledState;
+
 void checkBtnState() {
-    // check if isr detected a debounced flank
-    if (btnUpdated) {
-        btnUpdated = false;
-        // print the state of the button
-        Serial.println("{\"ButtonState\":" + String(btnState) + "}");
-    }
+  // check if isr detected a debounced flank
+  if (btnUpdated) {
+    btnUpdated = false;
+    // print the state of the button
+    Serial.println("{\"ButtonState\":" + String(btnState) + "}");
+  }
 }
 
 
@@ -23,45 +27,57 @@ static bool lastBtnState = false;
 unsigned long debounceDelay = 20;
 
 void btnChange() {
-    // update button with 20ms debounce on all flanks in isr
+  // update button with 20ms debounce on all flanks in isr
 
-    unsigned long now = millis();
-    if (now - lastDebounceTime > debounceDelay) {
-        btnState = !digitalRead(knop);
-        if (btnState != lastBtnState) {
-            lastBtnState = btnState;
-            btnUpdated = true;
-            lastDebounceTime = millis();
-        }
+  unsigned long now = millis();
+  if (now - lastDebounceTime > debounceDelay) {
+    btnState = !digitalRead(knop);
+    if (btnState != lastBtnState) {
+      lastBtnState = btnState;
+      btnUpdated = true;
+      lastDebounceTime = millis();
     }
+  }
 }
 
 
 void setup() {
-    Serial.begin(115200);
-    pinMode(knop, INPUT_PULLUP);
-    pinMode(led, OUTPUT);
-    // High == off (sinking led)
-    digitalWrite(led, HIGH);
-    attachInterrupt(knop, btnChange, CHANGE);
+  Serial.begin(115200);
+  pinMode(knop, INPUT_PULLUP);
+  pinMode(led, OUTPUT);
+  // High == off (sinking led)
+  digitalWrite(led, HIGH);
+  attachInterrupt(knop, btnChange, CHANGE);
 }
 
+
 void loop() {
-    checkBtnState(); // display the button state
-    if (Serial.available() != 0) {
-        String readString = Serial.readStringUntil('\n');
-        readString.trim();
-        if (readString == "IDENTIFY") {
-            Serial.println(R"({"id":")" + type + "\"}");
-        }
-
-        // button led, sinking OUTPUT
-        else if (readString == "ON") {
-            digitalWrite(led, LOW);
-        }
-        else if (readString == "OFF") {
-            digitalWrite(led, HIGH);
-        }
+  checkBtnState();  // display the button state
+  if (Serial.available() != 0) {
+    String readString = Serial.readStringUntil('\n');
+    readString.trim();
+    if (readString == "IDENTIFY") {
+      Serial.println(R"({"id":")" + type + "\"}");
     }
-
+    // button led, sinking OUTPUT
+    else if (readString == "ON") {
+      ledOn = true;
+    } else if (readString == "OFF") {
+      ledOn = false;
+    }
+  }
+  if (!ledOn) {
+    digitalWrite(led, HIGH);
+  } else {
+    if (millis() - millisOld > 500) {
+      if (ledState) {
+        digitalWrite(led, HIGH);
+        ledState = false;
+      } else {
+        digitalWrite(led, LOW);
+        ledState = true;
+      }
+      millisOld = millis();
+    }
+  }
 }
