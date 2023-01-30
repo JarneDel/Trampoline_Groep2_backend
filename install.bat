89c44bd7b51311080a0a0157687426b00d8f0409
jarneDel <jarne.delarue@student.howest.be>
2023-01-30 14:30:55 +0100

    fix: check if programs are installed

--- install.bat
+++ install.bat
@@ -9,19 +9,43 @@ if errorlevel 1 (
 ) else (
     echo node found
 )
-
-
-if not exist Trampoline_Groep2_backend (
-    echo cloning repo
-    git clone --single-branch -b main https://github.com/JarneDel/Trampoline_Groep2_backend.git
+:: check if git exists
+where git
+if errorlevel 1 (
+    echo git not found
+    echo please install git: https://git-scm.com/downloads and try again
+    pause
+    exit
 )
-cd Trampoline_Groep2_backend
+:: check if npm exists
+where npm
+if errorlevel 1 (
+    echo npm not found
+    echo please install npm: https://www.npmjs.com/get-npm and try again
+    pause
+    exit
+)
+
 echo pulling latest changes
 git pull
 echo installing node modules
 call npm install
 echo creating secrets file
+:: check if argument 1 exists
+if not defined 1 (
+    echo argument 1 does not exist
+    echo please provide a password for the database
+    pause
+    exit
+)
 echo SQL_PASSWORD=%1\n > .env
+:: check if argument 2 exists
+if not defined 2  (
+    echo argument 2 does not exist
+    echo please provide the database name for the mysql server
+    pause
+    exit
+)
 echo DB_NAME=%2 >> .env
 echo starting server
 call npm run start
